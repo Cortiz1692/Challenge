ad5ecbc41bf19670d882fc6b479e9a75dae57e12
Chrisitan <99914326+Cortiz1692@users.noreply.github.com>
2024-12-01 21:02:04 -0300

    Revert "upload challenge meli"

--- ChallengeMeli/back-end/poi-service/src/main/java/com/portalInmobiliario/poi_service/models/dao/PropertyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.portalInmobiliario.poi_service.models.dao;
-
-import com.portalInmobiliario.poi_service.models.documents.Property;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface PropertyRepository extends ReactiveMongoRepository<Property, String>{
-
-}
